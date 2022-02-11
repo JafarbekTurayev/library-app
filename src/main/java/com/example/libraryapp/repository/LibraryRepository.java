8ea2c4c13be2e66bcd405a933dfc1ef8786946d3
To'rayev Ja'farbek Uchqun o'gli <tjafarbek@mail.ru>
2022-02-11 11:28:44 +0500

    xato

--- src/main/java/com/example/libraryapp/repository/LibraryRepository.java
+++ src/main/java/com/example/libraryapp/repository/LibraryRepository.java
@@ -13,7 +13,7 @@ public interface LibraryRepository
 
     List<Library> findAllByNameContainsOrAddress_CityContains(String name, String city);
 
-    @Query(value = "select * from library where id=:ketmon_id and name like :ketmon", nativeQuery = true)
-    List<Library> getAllLib(@Param("ketmon_id") Integer id, @Param("ketmon_name") String name);
+//    @Query(value = "select * from library where id=:ketmon_id and name like :ketmon", nativeQuery = true)
+//    List<Library> getAllLib(@Param("ketmon_id") Integer id, @Param("ketmon_name") String name);
 
 }
